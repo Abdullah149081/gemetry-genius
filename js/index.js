4d25a7da59ba999b045b6d843acb845f3bba65be
Abdullah149081 <masudmd425@gmail.com>
2023-02-18 09:34:29 +0600

    Triangle geometry completed

--- js/index.js
+++ js/index.js
@@ -1,3 +1,49 @@
-document.getElementById("blog-btn").addEventListener("click", function () {
-  window.location.href = "blog.html";
+let serialNO = 0;
+
+document.getElementById("triangle-btn").addEventListener("click", function () {
+  serialNO += 1;
+
+  const inputValueB = inputValue("input-tri-b");
+  const inputValueH = inputValue("input-tri-h");
+
+  if (inputValueB == "" || inputValueH == "" || isNaN(inputValueB) || isNaN(inputValueH) || inputValueB <= 0 || inputValueH <= 0) {
+    return alert("Sorry,,, Please Input Valid Number");
+  }
+
+  const area = 0.5 * inputValueB * inputValueH;
+  const triangleTitle = geometryTitleName("tri-title");
+
+  setArea(triangleTitle, area);
 });
+
+
+
+
+
+
+
+// Utility function
+
+function inputValue(inputFieldId) {
+  const inputValueGet = document.getElementById(inputFieldId).value;
+  return inputValueGet;
+}
+
+function geometryTitleName(titleName) {
+  const titleNameGet = document.getElementById(titleName).innerText;
+  return titleNameGet;
+}
+
+function setArea(titleName, areaCal) {
+  const tbodyContainer = document.getElementById("tbody-container");
+  const trTag = document.createElement("tr");
+
+  trTag.innerHTML = `
+              <td>${serialNO}</td>
+              <td>${titleName}</td>
+              <td>${areaCal}cm<sup>2</sup></td>
+              <td><button class="btn lowercase">convert to m <sup>2</sup></button></td>
+  `;
+
+  tbodyContainer.appendChild(trTag);
+}
